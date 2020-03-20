e95ec4690a3dc40db929af6bb8ac348b2fdd9d8f
EgorYackovleff <Egor.yackovleff@gmail.com>
2020-03-20 22:39:23 +0300

    исправляем ошибку работы потока с неопределенной областью данных

--- Proj1/Proj1/Source.cpp
+++ Proj1/Proj1/Source.cpp
@@ -1,11 +1,37 @@
+#include <thread>
 
+void do_something(int& i)
+{
+	++i;
+}
+
+struct func
+{
+	int& i;
+
+	func(int& i_) :i(i_) {}
+
+	void operator()()
+	{
+		for (unsigned j = 0; j < 10000000; ++j)
+		{
+			do_something(i);
+		}
+	}
+};
+
+
+void oops()
+{
+	int some_local_state = 0;
+	func my_func(some_local_state);
+	std::thread my_thread(my_func);
+	my_thread.join();
+	//my_thread.detach();
+}
 
 int main()
 {
-
-
-
-
-
-	return 0;
+	oops();
 }
+
